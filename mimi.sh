5d65f548d4432b7b3ffcd47f970b49546907678e
Luc Boissaye <luc@boissaye.fr>
2023-03-28 16:18:38 +0200

    Add deadmansnitch

--- mimi.sh
+++ mimi.sh
@@ -2,9 +2,10 @@
 token=$(curl -s -X POST "https://api.netatmo.com/oauth2/token" -H "Content-Type: application/x-www-form-urlencoded;charset=UTF-8" -d "grant_type=refresh_token&refresh_token=$REFRESH_TOKEN&client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET" | jq -r ".access_token")
 out_of_sight=$(curl -s -X GET "https://api.netatmo.com/api/homestatus?home_id=$HOME_ID" -H "accept: application/json" -H "Authorization: Bearer $token" | jq ".body.home.persons[0].out_of_sight")
 echo "OUT_OF_SIGHT=$out_of_sight"
-if [ "$out_of_sight" = "false" ]; then
+if [ "$out_of_sight" = "true" ]; then
   echo "ERROR !"
   exit 1
 else
+  curl -d "m=presence_ok" "https://nosnch.in/$DEADMANSNITCH_ID"
   exit 0;
 fi;
